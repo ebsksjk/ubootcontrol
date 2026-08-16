2090ccc4716b57b69a586205ff468e6ae4c1ece4
mara@thessa <mara@thessa>
2026-08-16 10:23:08 +0200

    refactoring

--- util.py
+++ util.py
@@ -5,48 +5,43 @@ class Command:
     def __init__(self, req):
         self.request = req
 
+    def make_request(self, ip, port, sock, struct_format=None):
+        sock.sendto(self.request, (ip, port))
+        data = sock.recv(1024)
+        if struct_format:
+            return struct.unpack(struct_format, data)
+        return data
 
 class LAN_GET_SERIAL_NUMBER(Command):
     def __init__(self):
         super().__init__(bytes([0x04,0x00,0x10,0x00]))
 
     def make_request(self, ip, port, sock) -> int:
-        sock.sendto(self.request, (ip, port))
-        data = sock.recv(4096)
-        res = struct.unpack("<cccci", data)
-
-        return res[4]
+        data = super().make_request(ip, port, sock, struct_format="<cccci")
+        return data
 
 class LAN_X_GET_VERSION(Command):
     def __init__(self):
         super().__init__(bytes([0x07,0x00,0x40,0x00,0x21,0x21,0x00]))
 
     def make_request(self, ip, port, sock):
-        sock.sendto(self.request, (ip, port))
-        data = sock.recv(1024)
-        res = struct.unpack("<9c", data)
-        return res
+        data = super().make_request(ip, port, sock, struct_format="<9c")
+        return data
 
 class LAN_X_GET_STATUS(Command):
     def __init__(self):
         super().__init__(bytes([0x07,0x00,0x40,0x00,0x21,0x24,0x05]))
 
     def make_request(self, ip, port, sock):
-        sock.sendto(self.request, (ip, port))
-        data = sock.recv(1024)
-
+        # return data for now. i don't know how to unpack it yet
+        data = super().make_request(ip, port, sock, struct_format=None)
         return data
-        res = struct.unpack("<9c", data)
-        return res
 
 class LAN_X_SET_TRACK_POWER_OFF(Command):
     def __init__(self):
         super().__init__(bytes([0x07,0x00,0x40,0x00,0x21,0x80,0xa1]))
 
     def make_request(self, ip, port, sock):
-        sock.sendto(self.request, (ip, port))
-        data = sock.recv(1024)
-
+        # return data for now. i don't know how to unpack it yet
+        data = super().make_request(ip, port, sock, struct_format=None)
         return data
-        res = struct.unpack("<9c", data)
-        return res
